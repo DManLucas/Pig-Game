ae44586e5802d8173923a5cb32737a0907406c63
Lucas Nakeli <daddynakeli@gmail.com>
2020-08-02 21:19:09 +0200

    DOne

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0
-activePlayer = 0;
+init();
 
-/***use Math.floor function to return 
- * integer and not decimal since Math.Random 
- *returns a number between 0 and 1 but times by 6 to
- do from 0 and 5 then add 1 to get 1 to 6
- */
 
-document.querySelector('.dice').style.display = 'none';  
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 
 //annonymous function is a function without a name that cannot be reused
@@ -33,15 +21,64 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     }else{
         //next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    //add score 
+    scores[activePlayer] += roundScore;
+    
+    //update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    //check who won
+    if(scores[activePlayer] >=100){
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    }else{
+        nextPlayer();
+    }
+});
 
+function nextPlayer(){
+    //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+    
+
+function init(){
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    /***use Math.floor function to return 
+    * integer and not decimal since Math.Random 
+     *returns a number between 0 and 1 but times by 6 to
+    do from 0 and 5 then add 1 to get 1 to 6
+    */
+
+    document.querySelector('.dice').style.display = 'none';  
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+}
