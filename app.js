987f78f864f1e08900e666f2be048840aecd1c58
Lucas Nakeli <daddynakeli@gmail.com>
2020-08-02 17:53:36 +0200

    Active player

--- app.js
+++ app.js
@@ -27,7 +27,22 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDom.src = 'dice-' + dice + '.png';
 
     //3. Update round score IF rolled number isn't 1
+    if(dice !== 1){
+        //score will be added
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else{
+        //next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+    }
 });
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
+
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
